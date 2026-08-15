8db06ca88bb5977882a9866e13637207d3ace141
Firmansyah <111690548+Firmansyah0415@users.noreply.github.com>
2026-08-16 00:09:30 +0800

    fix: menambahkan route storage di web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/buat-storage', function () {
+    $targetFolder = storage_path('app/public');
+    // $_SERVER['DOCUMENT_ROOT'] akan otomatis mencari folder root internet Hostinger (public_html)
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
+
+    if (file_exists($linkFolder)) {
+        return 'Folder storage sudah ada. Hapus dulu yang lama.';
+    }
+
+    try {
+        symlink($targetFolder, $linkFolder);
+        return 'SUKSES! Symlink berhasil dibuat di: ' . $linkFolder;
+    } catch (\Exception $e) {
+        return 'GAGAL: ' . $e->getMessage();
+    }
+});
