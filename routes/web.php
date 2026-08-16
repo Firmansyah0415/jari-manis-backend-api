401135956e426adbd6a431f8af07229e5ac4efbf
alwi <210209501011@student.unm.ac.id>
2026-08-16 18:48:52 +0800

    storage link

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/generate-symlink', function () {
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return 'Symlink berhasil dibuat!';
+});
